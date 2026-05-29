5ea8c03ee9c00b9e00925b818d7d288e97acbaf3
ArmBagus <bagusaufaariqarmeidi_2428250059@mhs.mdp.ac.id>
2026-05-30 00:35:43 +0700

    fav n search screen (still maintainable)

--- cafetarium/lib/main.dart
+++ cafetarium/lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:cafetarium/screens/favorite_screen.dart';
+import 'package:cafetarium/screens/search_screen.dart';
 import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -12,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      home: const SearchScreen(),
     );
   }
 }
