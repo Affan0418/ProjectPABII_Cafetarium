bfd4a81b3dd91aef0656f4c215ab2c5b950c7b50
Muhammad Krisna Pramudya <krisnapramudya7@gmail.com>
2026-06-02 11:30:33 +0700

    Update detail_screen.dart

--- cafetarium/lib/screens/detail_screen.dart
+++ cafetarium/lib/screens/detail_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:cafetarium/screens/review_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
